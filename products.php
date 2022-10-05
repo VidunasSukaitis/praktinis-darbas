282b40f2579d23613718657a89bf867284add16d
VidunasSukaitis <112963662+VidunasSukaitis@users.noreply.github.com>
2022-10-05 17:54:00 +0300

    Initial commit

---
+++ products.php
@@ -0,0 +1,18 @@
+<?php
+
+// require_once('./classes/Product.php');
+
+// TODO: add products list page
+// $products = Product::getProducts('./data/products.json');
+?>
+
+<html>
+  <head>
+    <title>
+      Products list
+    </title>
+  </head>
+  
+  <body>
+  </body>
+</html>
